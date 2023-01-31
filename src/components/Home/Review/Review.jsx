71f3f26df4a01bd753a4a8c2962a20880e47c4b8
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-01-31 09:25:28 +0100

    youtube video and one video for all review section

--- src/components/Home/Review/Review.jsx
+++ src/components/Home/Review/Review.jsx
@@ -2,14 +2,61 @@ import {React, useState, useEffect} from 'react'
 import Dots from './Dots'
 import ReviewCards from './ReviewCards'
 import ReviewData from './ReviewData'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnglesRight, faAnglesLeft, faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
 
-
+import YouTube from "react-youtube";
 
 
 
 const Review = () => {
     const [currentSlide, setCurrentSlide] = useState(0)
     const [slideLength, setSlideLength] = useState(0)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const element1 = <FontAwesomeIcon icon={faPlay} color='white' size='xl' title='Play' className='group-hover:text-black cursor-pointer transition-colors duration-500'/>
+    const element2 = <FontAwesomeIcon icon={faPause} color='white' size='xl' title='Pause' className='group-hover:text-black cursor-pointer transition-colors duration-500'/>
+    // let vid = document.getElementById('youtube-video')
+    const [player, setPlayer] = useState(null)
+    
+    const onPlayerReady = (event) => {
+        setPlayer(event.target);
+        player.pauseVideo();
+      };
+    const onReady = (event) => {
+        setPlayer(event.target);
+    };
+
+    //   const onPlayerStateChange = (event) => {
+    //     setPlayer(event.target)
+    //     player.playVideo();
+    //   };
+    
+      const options = {
+        height: '100%',
+        width: '100%',
+        playerVars: {
+          autoplay: 0,
+          controls: 1,
+          playbar: 0
+        },
+      };
+
+    const controlVideo = () =>{
+        // const vid = event.target;
+        // isPlaying ? vid.pauseVideo() : vid.playVideo()
+        // setIsPlaying(!isPlaying)
+        // // console.log(isPlaying);
+        if (isPlaying) {
+            player.pauseVideo();
+          } else {
+            player.playVideo();
+          }
+          setIsPlaying(!isPlaying);
+    }
+
+    useEffect(() => {
+        console.log(isPlaying);
+    }, [isPlaying])
 
     useEffect(() => {
         setSlideLength(ReviewData.length)
@@ -53,9 +100,26 @@ const Review = () => {
 
   return (
     <main className='relative w-full bg-red1x/10 pb-10 xl:pb-0'>
-        <section className='flex flex-row w-full mx-auto xl:mx-0 overflow-x-hidden overflow-y-hidden flex-nowrap h-fit' id='slide_wrap'>
-            {reviewDataSet}
-        </section>
+      <section className="grid grid-col-1 lg:grid-cols-2 auto-rows-fr auto-cols-fr">
+            <section className='flex flex-row w-full mx-auto xl:mx-0 overflow-x-hidden overflow-y-hidden flex-nowrap h-fit' id='slide_wrap'>
+                    {reviewDataSet}
+            </section>
+            <section className='col-span-1 px-4 min-h-full pt-3 relative group'>
+                        <YouTube
+                                id='youtube-video'
+                                className='min-h-full h-full'
+                                videoId="pkwTpdpB8do"
+                                opts={options}
+                                onPlay={() => setIsPlaying(true)}
+                                onPause={() => setIsPlaying(false)}
+                                onReady={onReady}
+                            />
+                        <button type='button' className={`play-video group bg-red1x flex items-center justify-center h-16 w-16 rounded-fiftyPercent absolute top-fiftyPercent -translate-x-fiftyPercent -translate-y-fiftyPercent left-fiftyPercent z-20 transition-opacity duration-500 ${isPlaying ? "opacity-0 group-hover:opacity-100" : "opacity-100"}`} onClick={controlVideo}>
+                            {isPlaying ? element2 : element1}
+                        </button>
+            </section>
+      </section>
+       
         <section className='px-10 lg:px-28 dot-wrap pt-10 lg:pt-0 lg:-translate-y-[300%] mx-auto lg:mx-0 w-fit h-fit flex flex-row items-center gap-4 lg:gap-3'>
             {dotItems.map((dotItem, index)=>{
                 return <Dots dotItem={dotItem} key={'dotItem'+index} changeSlide={changeSlide} currentSlide={currentSlide}/>
--- src/components/Home/Review/ReviewCards.jsx
+++ src/components/Home/Review/ReviewCards.jsx
@@ -10,6 +10,7 @@ const ReviewCards = ({id, review, img, user, job, video, poster, currentSlide, s
     const element1 = <FontAwesomeIcon icon={faPlay} color='white' size='xl' title='Play' className='group-hover:text-black cursor-pointer transition-colors duration-500'/>
     const element2 = <FontAwesomeIcon icon={faPause} color='white' size='xl' title='Pause' className='group-hover:text-black cursor-pointer transition-colors duration-500'/>
     let vid = document.querySelector('video')
+    const [direction, setDirection] = useState('')
 
     const controlVideo = () =>{
         isPlaying ? vid.pause() : vid.play()
@@ -38,10 +39,10 @@ const ReviewCards = ({id, review, img, user, job, video, poster, currentSlide, s
         key={currentSlide}
         initial={{ x: 100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
-        exit={{ x: -100, opacity: 0 }}
+        exit={{ x: direction, opacity: 0 }}
         transition={{ duration: 0.5 }} 
-        className='inter min-w-full px-10 lg:pl-28 items-center gap-5 lg:gap-10 h-fit grid grid-cols-1 lg:grid-cols-2'>
-            <section className='py-28 space-y-7 col-span-1'>
+        className='inter min-w-full px-10 lg:pl-28 items-center gap-5 col-span-1 lg:gap-10 h-fit'>
+            <section className='py-28 space-y-7'>
                 <div className='relative lg:text-left'>
                     <h1 className='inter-400 sm:text-lg lg:text-xl'>{review}</h1>
                     <img src={Quotes} alt="quote" className='absolute w-16 -top-6 -left-6'/>
@@ -52,20 +53,14 @@ const ReviewCards = ({id, review, img, user, job, video, poster, currentSlide, s
                         <h1 className='inter-700 text-red1x  sm:text-lg'>{user}</h1>
                         <p className='inter-400 text-sm'>{job}</p>
                     </div>
+                    {/* {direction} */}
                     <button onClick={()=>currentSlide > 0 ? moveSlide(-1) : setCurrentSlide(slideLength-1)} className="hover:text-red1x inter inter-700 text-xl transition-colors duration-500 hover:bg-white hover:shadow-sm absolute top-[160%] lg:hidden left-0 -translate-x-fiftyPercent bg-white/80 h-12 w-12 flex items-center justify-center rounded-fiftyPercent"><FontAwesomeIcon icon={faAnglesLeft} className="text-red1x hover:scale-125 transition-transform ease-in-out duration-500" /></button>
                     <button onClick={()=>currentSlide < (slideLength-1) ? moveSlide(+1) : setCurrentSlide(0)} className="hover:text-red1x inter inter-700 text-xl transition-colors duration-500 hover:bg-white hover:shadow-sm absolute top-[160%] lg:hidden right-0 translate-x-fiftyPercent bg-white/80 h-12 w-12 flex items-center justify-center rounded-fiftyPercent"><FontAwesomeIcon icon={faAnglesRight} className="text-red1x hover:scale-125 transition-transform ease-in-out duration-500" /></button>
                     {/* <button onClick={()=>moveSlide(-1)} className="hover:text-red1x inter inter-700 text-xl transition-colors duration-500 hover:bg-white hover:shadow-sm absolute top-[160%] lg:hidden left-0 -translate-x-fiftyPercent bg-white/80 h-12 w-12 flex items-center justify-center rounded-fiftyPercent"><FontAwesomeIcon icon={faAnglesLeft} className="text-red1x hover:scale-125 transition-transform ease-in-out duration-500" /></button>
                     <button onClick={()=>moveSlide(1)} className="hover:text-red1x inter inter-700 text-xl transition-colors duration-500 hover:bg-white hover:shadow-sm absolute top-[160%] lg:hidden right-0 translate-x-fiftyPercent bg-white/80 h-12 w-12 flex items-center justify-center rounded-fiftyPercent"><FontAwesomeIcon icon={faAnglesRight} className="text-red1x hover:scale-125 transition-transform ease-in-out duration-500" /></button> */}
                 </div>
             </section>
-            <section className='col-span-1 h-full relative group'>
-                <video poster={poster} className='min-h-full mx-auto'>
-                    <source  src={video} type="video/mp4" />
-                </video>
-                <button type='button' className={`play-video group bg-red1x flex items-center justify-center h-16 w-16 rounded-fiftyPercent absolute top-fiftyPercent -translate-x-fiftyPercent -translate-y-fiftyPercent left-fiftyPercent z-20 transition-opacity duration-500 ${isPlaying ? "opacity-0 group-hover:opacity-100" : "opacity-100"}`} onClick={controlVideo}>
-                    {isPlaying ? element2 : element1}
-                </button>
-            </section>
+            
     </motion.section>
    </AnimatePresence>
   )
