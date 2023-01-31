915bb0a1cc791c0e1fb828d98545f6ce84a6957d
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-01-31 09:55:07 +0100

    hero animation for map and select

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,7 +3,7 @@ import Nav from '../Nav'
 import { Icon } from 'leaflet'
 import { MapContainer,  TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import "./../../../node_modules/leaflet/dist/leaflet.css";
-import SanfranAreas from './Map/SanFranAreas';
+import SanfranAreas from '../../data/Map/SanFranAreas';
 import $ from 'jquery';
 import { motion } from 'framer-motion';
 
@@ -41,13 +41,26 @@ const Hero = () => {
             opacity: 0,
             y:150,
             scale:0,
-            rotate:90
             },
         slideUp: { 
             opacity: 1, 
             y:0,
             scale:1,
-            rotate:0,
+            transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration:5,
+            }
+            },
+    }
+    const up = {
+        slideDown: { 
+            opacity: 0,
+            y:100,
+            },
+        slideUp: { 
+            opacity: 1, 
+            y:0,
             transition: {
             type: "spring",
             bounce: 0.4,
@@ -70,21 +83,21 @@ const Hero = () => {
             </div>
             <section className='w-full lg:w-sixtyPercent flex lg:justify-end mx-auto lg:mx-0'>
                 <div className='w-full poppins poppins-500 text-sm grid grid-rows-7 gap-8 grid-cols-1 place-items-center'>
-                    <div id="map" className="">
-                    <MapContainer center={position} zoom={18} scrollWheelZoom={false} icon={new Icon({iconUrl: './assets/images/icons/leaflet/images/marker-icon.png', iconSize:[20, 36], iconAnchor:[20, 36]})} className="xxs:w-60 xxs:h-60 xs:w-72 xs:h-72 w-80 h-80 sm:h-96 sm:w-96 max-w-sm rounded-ten">
-                        <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={position}>
-                            <Popup>
-                                Get property in the {area} area. <br /> Easily with <span className='text-red1x'>MLC</span>.
-                            </Popup>
-                        </Marker>
-                        <RecenterAutomatically position={position}/>                    
-                    </MapContainer>
-                    </div>
-                    <div className='flex flex-col sm:flex-row sm:justify-center gap-2 sm:gap-0 px-2 xxs:w-60 xs:w-72 w-80 max-w-sm sm:w-96 sm:px-5 py-4 col-span-1 bg-white border border-black/10 rounded-ten'>
+                    <motion.div variants={up} initial="slideDown" whileInView="slideUp" viewport={{once:true, amount:0}} id="map" className="rounded-ten">
+                        <MapContainer center={position} zoom={18} scrollWheelZoom={false} icon={new Icon({iconUrl: './assets/images/icons/leaflet/images/marker-icon.png', iconSize:[20, 36], iconAnchor:[20, 36]})} className="xxs:w-60 xxs:h-60 xs:w-72 xs:h-72 w-80 h-80 sm:h-96 sm:w-96 max-w-sm rounded-ten">
+                            <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={position}>
+                                <Popup>
+                                    Get property in the {area} area. <br /> Easily with <span className='up-red1x'>MLC</span>.
+                                </Popup>
+                            </Marker>
+                            <RecenterAutomatically position={position}/>                    
+                        </MapContainer>
+                    </motion.div>
+                    <motion.div variants={up} initial="slideDown" whileInView="slideUp" viewport={{once:true, amount:0}} className='flex flex-col sm:flex-row sm:justify-center gap-2 sm:gap-0 px-2 xxs:w-60 xs:w-72 w-80 max-w-sm sm:w-96 sm:px-5 py-4 col-span-1 bg-white border border-black/10 rounded-ten'>
                         <select name="types" id="types" className='cursor-pointer px-5 py-4 h-14 border border-lightGrey1x bg-lightGrey2x rounded-ten sm:rounded-l-ten sm:rounded-r-none'>
                             <option value="All Type" disabled selected>All Type</option>
                         </select>
@@ -100,7 +113,7 @@ const Hero = () => {
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M13.9901 12.3188L18.2939 16.6226C18.4452 16.7739 18.5301 16.9792 18.53 17.1932C18.53 17.4072 18.4449 17.6124 18.2935 17.7637C18.1421 17.915 17.9368 17.9999 17.7228 17.9998C17.5088 17.9998 17.3036 17.9147 17.1523 17.7633L12.8485 13.4595C11.562 14.456 9.9441 14.9249 8.32406 14.7709C6.70401 14.6169 5.20349 13.8515 4.12774 12.6304C3.05199 11.4094 2.48182 9.82433 2.53323 8.19779C2.58463 6.57125 3.25375 5.02539 4.40446 3.87468C5.55517 2.72397 7.10104 2.05485 8.72757 2.00344C10.3541 1.95203 11.9391 2.5222 13.1602 3.59795C14.3813 4.6737 15.1467 6.17423 15.3007 7.79427C15.4547 9.41432 14.9858 11.0322 13.9893 12.3188H13.9901ZM8.93033 13.1995C10.2033 13.1995 11.4241 12.6938 12.3243 11.7937C13.2244 10.8936 13.7301 9.67272 13.7301 8.39974C13.7301 7.12676 13.2244 5.90592 12.3243 5.00579C11.4241 4.10565 10.2033 3.59997 8.93033 3.59997C7.65735 3.59997 6.43651 4.10565 5.53637 5.00579C4.63624 5.90592 4.13055 7.12676 4.13055 8.39974C4.13055 9.67272 4.63624 10.8936 5.53637 11.7937C6.43651 12.6938 7.65735 13.1995 8.93033 13.1995Z" fill="white"/>
                             </svg>
                         </button>
-                    </div>
+                    </motion.div>
                 </div>
             </section>
         </section>
