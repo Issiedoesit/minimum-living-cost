43f5f080639bee9cdb11fd96229d5c211e9fd386
Isioma Ekwemuka <105283590+Issiedoesit@users.noreply.github.com>
2023-01-31 17:58:51 +0100

    Remove z 10 landloard

--- src/components/Landlord/Landlord.jsx
+++ src/components/Landlord/Landlord.jsx
@@ -13,7 +13,7 @@ const Landlord = () => {
     <section className='h-screen relative'>
       <div className='bg-black px-5 sm:px-10 lg:px-28'><Nav /></div>
       <div className='flex flex-col items-center justify-center h-full z-0 px-10'>
-      <div className='inter text-center text-red1x/60 relative z-10'>           
+      <div className='inter text-center text-red1x/60 relative'>           
       <FontAwesomeIcon icon={faHomeUser} className="text-red1x animate-wiggle z-10" size='7x'/>
         </div>
           <div className='inter text-center text-red1x/60 py-10 space-y-10'>
