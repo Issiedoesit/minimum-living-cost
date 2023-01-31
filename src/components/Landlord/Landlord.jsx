5c45953696737164f39649ac0e9c05e152f1bce3
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-01-31 17:42:48 +0100

    other pages

--- src/components/Landlord/Landlord.jsx
+++ src/components/Landlord/Landlord.jsx
@@ -13,9 +13,9 @@ const Landlord = () => {
     <section className='h-screen relative'>
       <div className='bg-black px-5 sm:px-10 lg:px-28'><Nav /></div>
       <div className='flex flex-col items-center justify-center h-full z-0 px-10'>
-        <div className='inter text-center text-red1x/60 relative -z-100'>
-           <FontAwesomeIcon icon={faHomeUser} className="text-red1x animate-wiggle give-z-10" size='7x'/>
-          </div>
+      <div className='inter text-center text-red1x/60 relative '>           
+      <FontAwesomeIcon icon={faHomeUser} className="text-red1x animate-wiggle z-10" size='7x'/>
+        </div>
           <div className='inter text-center text-red1x/60 py-10 space-y-10'>
             <h1 className='inter-700 text-3xl'>LANDLORD</h1>
             <p>Coming Soon</p>
--- src/components/Tenants/Tenants.jsx
+++ src/components/Tenants/Tenants.jsx
@@ -12,8 +12,8 @@ const Tenants = () => {
     <section className='h-screen relative'>
       <div className='bg-black px-5 sm:px-10 lg:px-28'><Nav /></div>
       <div className='flex flex-col items-center justify-center h-full z-0 px-10'>
-        <div className='inter text-center text-red1x/60 relative -z-100'>
-           <FontAwesomeIcon icon={faPersonWalkingLuggage} className="text-red1x animate-wiggle give-z-10" size='7x'/>
+      <div className='inter text-center text-red1x/60 relative '>
+         <FontAwesomeIcon icon={faPersonWalkingLuggage} className="text-red1x animate-wiggle z-10" size='7x'/>
           </div>
           <div className='inter text-center text-red1x/60 py-10 space-y-10'>
             <h1 className='inter-700 text-3xl'>Tenants</h1>
